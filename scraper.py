c5910388054325b467106e2e71aecbca27aec9c6
Jake Denekamp <jakeden1112@gmail.com>
2025-11-19 18:27:45 -0500

    added in new function that takes dictionary and puts it in csv so we don't have to make dictionary every time we run script

--- scraper.py
+++ scraper.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import re
+import csv
 '''
 For our first step, we will be using a preexisting dataset from hf, which we will simplify and make the model able to work with it
 Later on, we plan to create our own custom dataset that will have more questions (hf one only has half of questions asked)
 '''
-
-df = pd.read_json("hf://datasets/openaccess-ai-collective/jeopardy/data/train.jsonl", lines=True)
+def hf_dataframe():
+    df = pd.read_json("hf://datasets/openaccess-ai-collective/jeopardy/data/train.jsonl", lines=True)
+    return df
+df = hf_dataframe()
 
 print(df['answer'].count())
 print(df['answer'].nunique())
@@ -21,35 +24,59 @@ test = qa.loc[0,"question"]
 print(test)
 
 #adapted from google's ai response
-def cleaner(str):
-    clean = re.sub(r'[^\w\s]', '', str)
+def cleaner(text):
+    clean = re.sub(r'[^\w\s]', '', str(text))
+    clean = clean.lower()
+        
     return clean
 
 
 print(cleaner(test))
 
-#we want to pull out each word and how many times it shows up 
-#we will opt to remove common words that add minimal information (the, for, is, etc.)
-occDict = {}
-temp = ''
-for col in range(20):#qa['answer'].count()):
-    for ch in str(qa.loc[col,"question"]):
-        if ch == ' ':
+qa_clean = qa.map(cleaner)
+
+def occ_dict_maker (df, column):
+    #we want to pull out each word and how many times it shows up 
+    #we will opt to remove common words that add minimal information (the, for, is, etc.)
+    occDict = {}
+    temp = ''
+    for col in range(100):#df[column].count()):
+        for ch in str(df.loc[col,"question"]):
+            if ch == ' ':
+                if not("hrefhttpwwwjarchivecommedia" in temp):
+                    temp = temp.replace(' ', '')
+                    if temp in occDict:
+                        occDict[temp]+=1
+                    else:
+                        occDict[temp]=1
+                temp = ''
+                
+            temp = temp+ch
+            
+        if not("hrefhttpwwwjarchivecommedia" in temp):
+            temp = temp.replace(' ', '')
             if temp in occDict:
                 occDict[temp]+=1
             else:
                 occDict[temp]=1
-            temp = ''
-            
-        temp = temp+ch
-        
-    if temp in occDict:
-        occDict[temp]+=1
-    else:
-        occDict[temp]=1
-    temp = ''
+        temp = ''
 
-print(occDict)
+    return occDict
+
+occ_dict = occ_dict_maker(qa_clean,'question')
+print(occ_dict)
+
+#save the dictionary to a csv so we don't have to make it everytime
+def dict_to_csv (occ_dict):
+    with open('word_occurences.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['word','occurences'])
+        for key in occ_dict:
+            
+            writer.writerow([key, occ_dict[key]])
+
+
+dict_to_csv(occ_dict)
 
 
 
