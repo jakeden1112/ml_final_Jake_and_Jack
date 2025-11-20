7181244c1e31366037bbda090efe18e70a2e497f
Jake Denekamp <jakeden1112@gmail.com>
2025-11-19 23:30:45 -0500

    got a simple version of a knn model running, but not working (something is wrong with the one hot encoding)

--- .ipynb_checkpoints/scraper-checkpoint.py
+++ .ipynb_checkpoints/scraper-checkpoint.py
@@ -8,40 +8,36 @@ Later on, we plan to create our own custom dataset that will have more questions
 def hf_dataframe():
     df = pd.read_json("hf://datasets/openaccess-ai-collective/jeopardy/data/train.jsonl", lines=True)
     return df
-df = hf_dataframe()
 
-print(df['answer'].count())
-print(df['answer'].nunique())
-
-#print(df.head(20))
-
-qa = df[["question","answer"]]
-
-#print(qa.head(20))
-
-test = qa.loc[0,"question"]
-
-print(test)
 
 #adapted from google's ai response
 def cleaner(text):
     clean = re.sub(r'[^\w\s]', '', str(text))
     clean = clean.lower()
-        
+    clean = clean.replace('  ', ' ')
+    clean = clean.replace('  ', ' ')
+    clean = clean.replace('  ', ' ')
     return clean
 
 
-print(cleaner(test))
+def answer_dict_maker(df, column):
+    answerDict = {}
+    for col in range(df[column].count()):
+        temp = df.loc[col,column]
+        if temp in answerDict:
+            answerDict[temp]+=1
+        else:
+            answerDict[temp]=1
+    return answerDict
 
-qa_clean = qa.map(cleaner)
 
 def occ_dict_maker (df, column):
     #we want to pull out each word and how many times it shows up 
     #we will opt to remove common words that add minimal information (the, for, is, etc.)
     occDict = {}
     temp = ''
-    for col in range(100):#df[column].count()):
-        for ch in str(df.loc[col,"question"]):
+    for col in range(df[column].count()):
+        for ch in str(df.loc[col,column]):
             if ch == ' ':
                 if not("hrefhttpwwwjarchivecommedia" in temp):
                     temp = temp.replace(' ', '')
@@ -63,20 +59,80 @@ def occ_dict_maker (df, column):
 
     return occDict
 
-occ_dict = occ_dict_maker(qa_clean,'question')
-print(occ_dict)
+
 
 #save the dictionary to a csv so we don't have to make it everytime
-def dict_to_csv (occ_dict):
-    with open('word_occurences.csv', 'w', newline='') as file:
+def dict_to_csv (occ_dict, fname):
+    with open(fname, 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(['word','occurences'])
         for key in occ_dict:
             
             writer.writerow([key, occ_dict[key]])
 
+def create_basket (fname, low_bound, up_bound):
+    output_dict = {}
+    basket_list = []
+    with open(fname, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header if present
+        
+        for row in reader:
+            #we don't want to worry about words that are very uncommon and also words that are so common they are in almost every question
+            if int(row[1]) > low_bound and int(row[1]) < up_bound :
+                basket_list.append(row[0])
+                output_dict[row[0]] = int(row[1])
 
-dict_to_csv(occ_dict)
+        return basket_list, output_dict
+
+if __name__ == '__main__':
+
+    df = hf_dataframe()
+
+    print(df['answer'].count())
+    print(df['answer'].nunique())
+    
+    #print(df.head(20))
+    
+    qa = df[["question","answer"]]
+    
+    #print(qa.head(20))
+    
+    test = qa.loc[0,"question"]
+    
+    print(test)
+
+    print(cleaner(test))
+
+    qa_clean = qa.map(cleaner)
+
+    #print(qa_clean.head(20))
+
+    #answer_dict = answer_dict_maker(qa_clean, 'answer')
+    
+    #dict_to_csv(answer_dict, 'answer_occurences.csv')
+
+    basket, test_dict  = create_basket('Answer_occurences.csv',5,20000)
+
+    print(test_dict)
+    print(len(test_dict))
+    print(len(basket))
+    
+    '''
+    #occ_dict = occ_dict_maker(qa_clean,'question')
+    #print(occ_dict)
+    
+    #dict_to_csv(occ_dict, 'word_occurences.csv')
+    
+    basket, test_dict  = create_basket('word_occurences.csv',30,500)
+    
+    #print(test_dict)
+    print(len(test_dict))
+    print(len(basket))
+    '''
+    
+    
+    print("done")
 
 
 
--- model.py
+++ model.py
@@ -1,5 +1,90 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
+import scraper
+import numpy as np
+from datetime import datetime
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
-print("hello")
+
+
+
+if __name__ == '__main__':
+
+    print(datetime.now())
+
+    df = scraper.hf_dataframe()
+    
+    qa = df[["question","answer"]]
+    
+    qa_clean = qa.map(scraper.cleaner)
+        
+    #occ_dict = scraper.occ_dict_maker(qa_clean,'question')
+    
+    basket, word_dict  = scraper.create_basket('word_occurences.csv',30,500)
+
+    ans_list, ans_dict  = scraper.create_basket('Answer_occurences.csv',5,20000)
+
+    count = 1
+    X_list = []
+    y_list = []
+    for i in range(0,5):#len(qa_clean)):
+        if qa_clean.loc[i,"answer"] in ans_list:
+            tempVector = []
+            for j in range(0,len(basket)):
+                #something fishy here
+                if str(basket[j]) in qa.loc[i,"question"]:
+                    tempVector.append(1)
+                else:
+                    tempVector.append(0)
+            X_list.append(tempVector)
+            y_list.append(ans_list.index(qa_clean.loc[i,"answer"]))
+            print(count)
+            count+=1
+            
+    X = np.array(X_list)
+    y = np.array(y_list)
+
+    for a in range(0,len(X_list)):
+        print("X:", end = " ")
+        for b in range(0,len(X_list[a])):
+            if X_list[a][b] == 1:
+                print(basket[b], end = ", ")
+        print("Y: "+str(ans_list[a]))
+
+'''
+This is the output:
+
+X: arrest, theory, u, ali, leo, hi, e, g, p, f, ear, ears, le, d, po, o, w, y, rest, al, s, sing, r, h, m, si, ma, un, las, n, t, ho, der, sin, l, il, lil, ye, er, Y: copernicus 
+X: 1912, football, seasons, ball, foot, ed, e, p, 19, f, le, d, o, w, y, al, sons, season, s, b, r, h, m, n, t, ho, tar, v, ian, wit, ts, l, seas, di, ba, pi, isle, ants, ol, ant, ave, ia, Y: jim thorpe
+X: average, hours, sunshine, suns, u, hi, e, ours, shine, g, f, cord, ear, hour, d, o, y, s, co, r, h, m, ma, un, 55, ate, n, t, ho, rage, ha, v, era, 0, ye, ave, er, Y: arizona 
+X: 1963, burger, u, pan, let, 63, ed, hi, ill, e, g, p, 19, le, billion, d, o, w, y, et, s, co, ive, b, r, h, pa, m, ny, iv, n, t, ho, k, v, ts, l, il, lion, 96, serve, ink, er, Y: mcdonalds
+X: u, ted, ed, e, sec, g, p, f, d, o, s, co, r, h, m, si, frame, en, ate, resident, n, t, id, con, ram, er, Y: john adams
+
+*** something is wrong with basket
+'''
+        
+    
+    #print(np.array(y_list))
+
+    #for ans in y:
+        #print(ans_list[ans])
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    knn = KNeighborsClassifier(n_neighbors=1)
+
+    knn.fit(X_train, y_train)
+
+    y_pred = knn.predict(X_test)
+
+    print(y_pred)
+
+    
+    
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+
+    
+    print(datetime.now())    
+    print("done")
